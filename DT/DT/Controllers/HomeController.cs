3cf116b955eac9d55ee0a22a38fc3a0447610498
Igor Didenko <IDidenko@bn.com>
2016-10-26 14:38:15 -0400

    Added DT folder

---
+++ DT/DT/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DT.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Dyanamic Text. © Igor Didenko 1987-2016";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "";
+
+            return View();
+        }
+    }
+}
